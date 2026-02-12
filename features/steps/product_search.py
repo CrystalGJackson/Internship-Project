85cd09625876bdcbf9b7450991e5b890254e5670
Crystal Jackson <crystalgjackson1987@gmail.com>
2026-02-11 21:36:07 -0500

    Test 1 Internship

--- features/steps/product_search.py
+++ features/steps/product_search.py
@@ -30,3 +30,39 @@ def click_search_icon(context):
 def verify_found_results_text(context, search_word):
     assert search_word.lower() in context.driver.current_url.lower(), \
         f'Expected query not in {context.driver.current_url.lower()}'
+
+# Test 1 Internship
+
+@given('Open the main page')
+def open_google(context):
+    context.driver.get('https://soft.reelly.io')
+
+@when('Log in to the page')
+def click_login(context):
+    context.driver.get('https://soft.reelly.io/sign-in')
+    sleep(20)
+    context.driver.find_element(By.CSS_SELECTOR,"[class='form-header']").click()
+
+
+@then('Click on "settings" at the left side menu')
+def click_settings(context):
+    context.driver.find_element(By.CSS_SELECTOR,"[class='menu-button-block w-inline-block w--current']").click()
+    sleep(20)
+
+@then('Click on the verification option')
+def click_verification(context):
+    context.driver.find_element(By.CSS_SELECTOR,"[class='setting-text']").click()
+    sleep(20)
+
+@then('Verify the right page opens')
+def verify_right_page(context):
+    context.driver.find_element(By.CSS_SELECTOR,"[step-1-block']").click()
+    sleep(20)
+
+@then('Verify "upload image" and "Next step" buttons are available')
+def verify_next_step(context):
+    actual_text:context.driver.find_element(By.CSS_SELECTOR,"[class='upload-button-2']").text
+    sleep(20)
+    actual_text:context.driver.find_element(By.CSS_SELECTOR,"[class='button-verify margin-top-8 w-inline-block']").text
+    assert'verify "upload image" and "Next step" buttons are available' in 'actual_text',f'expected verify "upload image" and "Next step" buttons are available''text not in {actual_text}'
+    sleep(20)
--- features/tests/product_search.feature
+++ features/tests/product_search.feature
@@ -1,7 +1,15 @@
 Feature: Test Scenarios for Search functionality
 
-  Scenario: User can search for a product
-    Given Open Google page
-    When Input Car into search field
-    And Click on search icon
-    Then Product results for Car are shown
+#  Scenario: User can search for a product
+#    Given Open Google page
+#    When Input Car into search field
+#    And Click on search icon
+#    Then Product results for Car are shown
+
+    Scenario: User can click on verifications settings option and verify the right page opens
+      Given Open the main page
+      When Log in to the page
+      Then Click on "settings" at the left side menu
+      Then Click on the verification option
+      Then Verify the right page opens
+      Then Verify "upload image" and "Next step" buttons are available
